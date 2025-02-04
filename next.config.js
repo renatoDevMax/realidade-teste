2a5e2530b466898b9a5e0c4aea12c2a07a003273
renatoDevMax <renato.devmaximiano@gmail.com>
2025-02-04 10:34:37 -0300

    testando ra 2

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://aframe.io https://raw.githack.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; media-src 'self' blob:; connect-src 'self' blob:; worker-src blob:;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
